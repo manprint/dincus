5147112bfdf14bd6ef00c5df097c3d92317aee4c
Fabio <fabio@git.it>
2025-11-15 17:40:28 +0100

    bump start script version.

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ BRIDGE_NAME="dincus_br"
 SUBNET="10.10.155.0/30"
 GATEWAY="10.10.155.2"
 
-IMAGE="ghcr.io/manprint/dincus:1.0.0"
+IMAGE="ghcr.io/manprint/dincus:1.0.1"
 CONTAINER="dincus"
 CONTAINER_IP="10.10.155.1"
 
